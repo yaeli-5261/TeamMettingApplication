de2193086a7ff5f9f9f1331b059596daefd04926
yaeli <yaelina5261@gmail.com>
2025-05-22 00:58:55 +0300

    delete connectionstrings

--- MeetSummarizerReact/src/store/authSlice.tsx
+++ MeetSummarizerReact/src/store/authSlice.tsx
@@ -120,8 +120,8 @@ export const signIn = createAsyncThunk("Auth/login", async (user: { email: strin
       headers: {
         'Content-Type': 'application/json'
       }})
-    console.log("Login response:", res.data.User);
-    console.log("Login Token:", res.data.Token);
+    console.log("Login response:", res.data.user);
+    console.log("Login Token:", res.data.token);
 
 
     // שמירת הטוקן ב-cookie עם תאריך תפוגה ארוך (30 ימים)
