7bdc6100b883fdeb762b486d98102d6b997d25c0
yaeli <yaelina5261@gmail.com>
2025-05-23 02:42:43 +0300

    checked api AI

--- AIServer/server.py
+++ AIServer/server.py
@@ -130,7 +130,7 @@ def process_file():
     
 @app.route("/", methods=["GET"])
 def home():
-    return "Server is running", 200
+    return "Server is working!!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- MeetSummarizerReact/src/components/File/FileUploader.tsx
+++ MeetSummarizerReact/src/components/File/FileUploader.tsx
@@ -1,5 +1,4 @@
 "use client"
-// בה  עובד מושלם!!
 
 import type React from "react"
 import { useEffect, useState } from "react"
