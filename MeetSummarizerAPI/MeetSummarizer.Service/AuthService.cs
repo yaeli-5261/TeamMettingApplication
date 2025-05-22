e74a1ec45278c006f93732857a64522c0fc94dca
yaeli <yaelina5261@gmail.com>
2025-05-22 15:59:38 +0300

    JWT__KEY in s3SERVICE

--- MeetSummarizerAPI/MeetSummarizer.Service/AuthService.cs
+++ MeetSummarizerAPI/MeetSummarizer.Service/AuthService.cs
@@ -71,7 +71,9 @@ public class AuthService
 
     public string GenerateJwtToken(string username, string[] roles, int id)
     {
-        var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        //var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_KEY")));
+
         var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256);
 
         var claims = new List<Claim>
