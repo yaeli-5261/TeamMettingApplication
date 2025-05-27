08f96ab5b7f134002eac809d4bffdc7b4adc2db9
yaeli <yaelina5261@gmail.com>
2025-05-27 20:23:08 +0300

    add chat navigate in REACT

--- MeetSummarizerReact/src/router.tsx
+++ MeetSummarizerReact/src/router.tsx
@@ -7,6 +7,7 @@ import SignIn from "./components/login/SignIn";
 import AddMeetingForm from "./components/Meeting/AddMeetingForm";
 import MeetingDetails from "./components/Meeting/MeetingDetails";
 import UpdateMeetingDialog from "./components/Meeting/UpdateMeetingDialog";
+import TeamChat from "./components/chat/chat-message";
 // import { AppSidebar } from "./components/Pages/sideBar";
 
 
@@ -25,6 +26,8 @@ export const router = createBrowserRouter([
             { path: "login", element: <SignIn /> },
             { path: "add-meeting", element: <AddMeetingForm /> },
             { path: "meeting-details/:meetingId", element: < MeetingDetails/> },
+            { path: "chat", element:  <TeamChat/>},
+
           
 
         ],     
